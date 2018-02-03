fdcf85a0e0216fbadc1c5b1ce3d3aa2c3fbc4eef
Clément Bouscasse <clement@opensignal.com>
2018-02-03 01:59:48 +0000

    use pd.concat to ensure column order is preserved in tests

--- features/steps/dtypes.py
+++ features/steps/dtypes.py
@@ -21,22 +21,21 @@ def step_impl(context, column_levels, index_levels):
 
 @then("it matches a manually created data frame with correct dtypes")
 def step_impl(context):
-    all_dtypes_df = pd.DataFrame(data={
-        'integer_col': pd.Series([0, 1, 2], dtype=np.int),
-        'integer32_col': pd.Series([0, 10, 20], dtype=np.int32),
-        'integer64_col': pd.Series([0, 100, 200], dtype=np.int64),
-        'float_col': pd.Series([3.0, 4.1, 5.2], dtype=np.float),
-        'float32_col': pd.Series([3.0, 4.1, 5.2], dtype=np.float32),
-        'float64_col': pd.Series([3.0, 4.1, 5.2], dtype=np.float64),
-        'datetime_col': pd.Series(
+    all_dtypes_df = pd.concat([
+        pd.Series([0, 1, 2], dtype=np.int, name='integer_col'),
+        pd.Series([0, 10, 20], dtype=np.int32, name='integer32_col'),
+        pd.Series([0, 100, 200], dtype=np.int64, name='integer64_col'),
+        pd.Series([3.0, 4.1, 5.2], dtype=np.float, name='float_col'),
+        pd.Series([3.0, 4.1, 5.2], dtype=np.float32, name='float32_col'),
+        pd.Series([3.0, 4.1, 5.2], dtype=np.float64, name='float64_col'),
+        pd.Series(
             [datetime.datetime(2018, 2, 1), datetime.datetime(2018, 2, 2), datetime.datetime(2018, 2, 3)],
-            dtype=np.datetime64),
-        'datetime64_col': pd.Series(
+            dtype=np.datetime64, name='datetime_col'),
+        pd.Series(
             [datetime.datetime(2018, 2, 1), datetime.datetime(2018, 2, 2), datetime.datetime(2018, 2, 3)],
-            dtype=np.datetime64),
-        'object_col': pd.Series(['egg', 'spam', 'bacon'], dtype=object),
-        'str_col': pd.Series(['silly walks', 'spanish inquisition', 'dead parrot'], dtype=str),
-
-    })
+            dtype=np.datetime64, name='datetime64_col'),
+        pd.Series(['egg', 'spam', 'bacon'], dtype=object, name='object_col'),
+        pd.Series(['silly walks', 'spanish inquisition', 'dead parrot'], dtype=str, name='str_col'),
+    ], axis=1)
 
     pdt.assert_frame_equal(all_dtypes_df, context.parsed)
