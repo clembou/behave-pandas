8089697e466d0629dff515b4d2b38383bbf39aa5
Clément Bouscasse <clement@opensignal.com>
2020-02-09 19:02:16 +0000

    correct docstring

--- features/environment.py
+++ features/environment.py
@@ -8,8 +8,8 @@ def before_scenario(context, scenario):
 
 def should_skip_scenario_due_to_pandas_version(context, scenario):
     """
-    Allow to skip scenario for a given pandas version by decorating it with @skip.pandasversion_comparison
-    ie @skip.pandas>, @skipApi>25, @skipApi<24, @skipApi>=25, @skipApi<=29
+    Allow to skip scenario for a given pandas version:
+    @skip.before.pandasv1.
     """
     pandas_skip_tag = "skip.before.pandasv1"
 
