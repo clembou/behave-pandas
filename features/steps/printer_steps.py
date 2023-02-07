238e45612ef02b2d562664df98e42af98a39a0a3
Leo Kacenjar <lkacenjar@morrisanimalfoundation.org>
2023-02-06 17:19:00 -0700

    #17 Finally getting those prints where they belong.

--- features/steps/printer_steps.py
+++ features/steps/printer_steps.py
@@ -7,11 +7,11 @@ use_step_matcher("re")
 
 @then("it prints a valid string copy pasteable into gherkin files")
 def step_impl(context):
+    print(context.text)
+    print(context.result)
     assert context.result == context.text
 
 
 @step("printed using data_frame_to_table")
 def step_impl(context):
-    print(context.text)
     context.result = dataframe_to_table(context.parsed)
-    print(context.result)
