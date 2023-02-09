981c9400b97d6f32a929ef7d9effc16b94c08d6a
Clément Bouscasse <clement@opensignal.com>
2023-02-09 22:53:42 +0000

    remove upper bound restriction on pandas and numpy + changelog

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setuptools.setup(
     long_description=open("README.md", "r").read(),
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
-    install_requires=["behave", "pandas>=0.21,<1.5", "tabulate", "numpy<1.24"],
+    install_requires=["behave", "pandas>=0.21", "tabulate"],
     classifiers=[
         "Development Status :: 4 - Beta",
         "Programming Language :: Python",
